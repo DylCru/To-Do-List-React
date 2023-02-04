d37000324bca373a64548e12475891453618fbf9
CrzRain <dylancruickshank0@gmail.com>
2023-02-04 18:20:16 +0000

    Update import

--- cool-react-app/src/App.jsx
+++ cool-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import tasksData from './tasks'
-import Task from './Task'
+import Task from './components/Task'
 
 function App() {
   const [tasks, setTasks] = useState(tasksData) 
