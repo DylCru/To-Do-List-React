bc4958f0aa79a888d58c5fe64dc1a39713e2a62b
CrzRain <dylancruickshank0@gmail.com>
2023-02-04 18:56:39 +0000

    Set background colour to priority

--- cool-react-app/src/components/Task.jsx
+++ cool-react-app/src/components/Task.jsx
@@ -1,8 +1,21 @@
 import React, {useState} from "react";
 
 export default function task(props) {
+    let priorityColour;
+    switch(props.priority) {
+        case "high":
+            priorityColour = {backgroundColor: "red"}
+            break
+        case "medium":
+            priorityColour = {backgroundColor: "orange"}
+            break
+        case "low" :
+            priorityColour = {backgroundColor: "green"}
+    }
+
+
     return (
-        <div className="task">
+        <div className="task" style={priorityColour}>
             <h3 className="task--name">{props.task}</h3>
             <h4 className="task--priority">{props.priority}</h4>
         </div>
