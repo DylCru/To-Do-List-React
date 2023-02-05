66a8c5d0ad7c31f470ec73ec8d0c60e23a1ce190
BuildTools <unconfigured@null.spigotmc.org>
2023-02-05 13:43:19 +0000

    Refactor for styling

--- cool-react-app/src/components/AddTask.jsx
+++ cool-react-app/src/components/AddTask.jsx
@@ -3,14 +3,24 @@ import React from "react";
 export default function AddTask(props) {
     return (
     <div className="add-task">
-        <input type="text" name="task" value={props.newTask.task} onChange={props.handleChange} placeholder="Task Name"/>
-        <input type="radio" name="priority" value="Low" onChange={props.handleChange}/>
-        <label htmlFor="low">Low</label>
-        <input type="radio" name="priority" value="Medium" onChange={props.handleChange}/>
-        <label htmlFor="medium">Medium</label>
-        <input type="radio" name="priority" value="High" onChange={props.handleChange}/>
-        <label htmlFor="high">High</label>
+        <input className="task-name" type="text" name="task" value={props.newTask.task} onChange={props.handleChange} placeholder="Task Name"/>
+        
+        <div className="prio-select">
+            <div>
+                <input className="task-prio" type="radio" name="priority" value="Low" onChange={props.handleChange}/>
+                <label htmlFor="low">Low</label>
+            </div>
+            <div>
+                <input className="task-prio" type="radio" name="priority" value="Medium" onChange={props.handleChange}/>
+                <label htmlFor="medium">Medium</label>
+            </div>
+            <div>
+                <input className="task-prio" type="radio" name="priority" value="High" onChange={props.handleChange}/>
+                <label htmlFor="high">High</label>
+            </div>
+        </div>
+        
         <button onClick={props.addTask}>Add Task</button>
-      </div>
+    </div>
     )
 }
