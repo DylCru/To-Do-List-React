c47e629f12257065dfa4077c1bcf307a7f6afd1f
CrzRain <dylancruickshank0@gmail.com>
2023-02-05 18:27:02 +0000

    Add transition functionality

--- cool-react-app/src/components/AddTask.jsx
+++ cool-react-app/src/components/AddTask.jsx
@@ -7,27 +7,28 @@ export default function AddTask(props) {
         setShown(prev => !prev)
     }
 
+    const addClass = shown ? "new-task-show" : "new-task-hide"
+    
     return (
-    <div className="new-task">
+    <div className={addClass}>
         <div className="toggle" onClick={toggle}>{shown ? '⬆' : '⬇'}</div>
         <div className="add-task">
             <input className="task-name" type="text" name="task" value={props.newTask.task} onChange={props.handleChange} placeholder="Task Name"/>
-            
-            <div className="prio-select">
-                <div>
-                    <input className="task-prio" type="radio" name="priority" value="Low" onChange={props.handleChange}/>
-                    <label htmlFor="low">Low</label>
-                </div>
-                <div>
-                    <input className="task-prio" type="radio" name="priority" value="Medium" onChange={props.handleChange}/>
-                    <label htmlFor="medium">Medium</label>
-                </div>
-                <div>
-                    <input className="task-prio" type="radio" name="priority" value="High" onChange={props.handleChange}/>
-                    <label htmlFor="high">High</label>
-                </div>
-            </div>
-            
+
+                <div className="prio-select">
+                    <div>
+                        <input className="task-prio" type="radio" name="priority" value="Low" onChange={props.handleChange}/>
+                        <label htmlFor="low">Low</label>
+                    </div>
+                    <div>
+                        <input className="task-prio" type="radio" name="priority" value="Medium" onChange={props.handleChange}/>
+                        <label htmlFor="medium">Medium</label>
+                    </div>
+                    <div>
+                        <input className="task-prio" type="radio" name="priority" value="High" onChange={props.handleChange}/>
+                        <label htmlFor="high">High</label>
+                    </div>
+                </div>    
             <button onClick={props.addTask}>Add Task</button>
         </div>
     </div>
